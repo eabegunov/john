b7a9ca95b01aa5dee6d8780b8c0082f7704c6fd5
eabegunov <evgeney_pr@mail.ru>
2023-01-14 18:08:37 +0300

    edit server.py

--- server.py
+++ server.py
@@ -1,2 +1,29 @@
-import socket
+import socket, threading
 
+LOCALHOST = "127.0.0.1"
+PORT = 3030
+
+server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+
+server.bind((LOCALHOST, PORT))
+print("Сервер запущен!")
+
+class ClientThread(threading.Thread):
+
+    def __init__(self,clientAddres, clientsocket):
+        threading.Thread.__init__(self)
+        self.csocket = clientsocket
+        print("Новое подключение: ", clientAddres)
+    def run(self):
+        msg = ''
+        while True:
+            data = self.csocket.recv(4096)
+            msg = data.decode()
+            print(msg)
+
+while True:
+    server.listen(1)
+    clientsock, clientAddress = server.accept()
+    newthread = ClientThread(clientAddress, clientsock)
+    newthread.start()
